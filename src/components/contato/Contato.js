e554e9f3e11525f0e26a51305c879a1626f2e4c7
Marco Antonio <114794163+MarcoAgostinoo@users.noreply.github.com>
2024-05-10 04:00:34 +0000

    ajustcontatoresponsive

--- src/components/contato/Contato.js
+++ src/components/contato/Contato.js
@@ -4,25 +4,35 @@ export default function Contato() {
   return (
     <div className={styles.container}>
       <section className={styles.section}>
-        <h2 className={styles.title}>LC Copper</h2>
+        <h2 className={styles.title}>Contato</h2>
         <div className={styles.info}>
           <p className={styles.paragraph}>
-            <strong>Escritório e Fábrica:</strong><br/>
-            Rua São João, 200 | Jardim Soeiro<br/>
-            Ferraz de Vasconcelos - SP<br/>
+            <strong>Escritório e Fábrica:</strong>
+            <br />
+            Rua São João, 200 | Jardim Soeiro
+            <br />
+            Ferraz de Vasconcelos - SP
+            <br />
             CEP 08540-200
           </p>
           <p className={styles.paragraph}>
-            <strong>Telefone:</strong> (11) 4676-0398 / 4676-3092<br/>
-            <strong>Fax:</strong> (11) 4679-7171<br/>
+            <strong>Telefone:</strong> (11) 4676-0398 / 4676-3092
+            <br />
+            <strong>Fax:</strong> (11) 4679-7171
+            <br />
             <strong>Celular:</strong> (11) 94789-5483
           </p>
           <p className={styles.paragraph}>
             <strong>Engenheiro Comercial:</strong> Eduardo Giuliatti Teixeira
           </p>
           <p className={styles.paragraph}>
-            <strong>E-mail:</strong> <a href="mailto:fernando@lccopper.com.br">fernando@lccopper.com.br</a><br/>
-            <strong>Site:</strong> <a href="http://www.lccopper.com.br">www.lccopper.com.br</a>
+            <strong>E-mail:</strong>{" "}
+            <a href="mailto:fernando@lccopper.com.br">
+              fernando@lccopper.com.br
+            </a>
+            <br />
+            <strong>Site:</strong>{" "}
+            <a href="http://www.lccopper.com.br">www.lccopper.com.br</a>
           </p>
         </div>
       </section>
