e23d05a325cf0042f63dd60bb8b009d5a6224eb4
unknown <marco.agostinoo@hotmail.com>
2024-05-26 07:43:57 -0300

    ajustfonth1inproducts

--- src/components/products/Eletrodos.tsx
+++ src/components/products/Eletrodos.tsx
@@ -4,8 +4,8 @@ export default function Eletrodos() {
   return (
     <div className={styles.container}>
       <div className={styles.texttop}>
-        <div>
-          <h1 className={styles.title}>
+        <div className={styles.title}>
+          <h1>
             Eletrodos de Solda por Resistência: A Solução para Alta
             Produtividade e Baixo Custo
           </h1>
