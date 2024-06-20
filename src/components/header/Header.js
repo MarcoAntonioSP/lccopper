acea5a8319efc90b5eb46a20c5b82b314630ba39
unknown <marco.agostinoo@hotmail.com>
2024-06-20 09:21:44 -0300

    newphonewhatsapp996016574

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -54,7 +54,7 @@ export default function Header() {
         </nav>
       </div>
       <div className=""></div>
-      <a href="https://wa.me/5511940238626?text= Olá, tudo bem?" target="_blank">
+      <a href="https://wa.me/5511996016574?text= Olá, tudo bem?" target="_blank">
         <img src="/whatsAppicone.png" className={styles.whatsicon} alt="" />
       </a>
     </div>
