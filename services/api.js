042831e82f60fbe25fb8414d2013e15d4a00a9fd
Sandro Santos <sandro.santos@radialdc.local>
2024-10-24 02:54:32 -0300

    ajustaxios321321

--- services/api.js
+++ services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api-vagas-lccopper-nvrfqlz9q-marco-antonios-projects-796d869d.vercel.app/', // Certifique-se de que o backend Express está rodando na porta 3006
+  baseURL: 'https://api-vagas-lccopper.vercel.app/', // Certifique-se de que o backend Express está rodando na porta 3006
   headers: {
     'Content-Type': 'application/json',
   }
