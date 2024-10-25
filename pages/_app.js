a3167946b22f4d70241ade5f7f90141ecb5c2851
unknown <marco.agostinoo@hotmail.com>
2024-10-25 10:27:39 -0300

    daskldjakdjakdjka

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import { extendTheme } from '@chakra-ui/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import Script from 'next/script';
@@ -18,32 +17,8 @@ const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Head>
-        <meta
-          name="keywords"
-          content="metalurgia, reposição de solda, usinagem, indústria automotiva, LC Copper Metalúrgica, materiais ferrosos, materiais não ferrosos, soldagem a ponto, projeto de solda, Ferraz de Vasconcelos, São Paulo"
-        />
-        <meta
-          name="description"
-          content="A LC Copper Metalúrgica Eireli é referência em reposição de solda e usinagem de materiais ferrosos e não ferrosos, atendendo principalmente a indústria automotiva. Com quase 25 anos de experiência, garantimos qualidade e precisão em nossos serviços."
-        />
-        <title>LC Copper Metalúrgica Eireli - Soluções em Metalurgia</title>
-        <link rel="canonical" href="http://www.lccopper.com/" />
-        <meta name="robots" content="index, follow" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
-        />
-      </Head>
-
+    <ChakraProvider>
       <Component {...pageProps} />
-
-      {/* Carregamento dos scripts do Bootstrap (sem a necessidade de jQuery ou Popper separadamente) */}
-      <Script 
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-        strategy="afterInteractive"
-      />
     </ChakraProvider>
   );
 }
